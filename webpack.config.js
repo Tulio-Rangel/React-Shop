2b1c91233d153c65272e782b8d4c8c10dd3b3aff
Tulio Rangel <tjrn20@hotmail.com>
2021-12-26 16:59:36 -0500

    solve webpack.config rules/loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: "./src/index.js",
@@ -18,7 +19,9 @@ module.exports = {
                 exclude: /node_modules/,
                 use: {
                     loader: "babel-loader"
-                },
+                }
+            },
+            {
                 test: /\.html$/,
                 use: [
                     {
