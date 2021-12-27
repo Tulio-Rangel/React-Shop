493f8629d75f2d36dddf59864141e8be3dcb6c8c
Tulio Rangel <tjrn20@hotmail.com>
2021-12-27 18:54:02 -0500

    add login component

---
+++ src/containers/login.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div className="login">
+        <div className="form-container">
+          <img src="./logos/logo_yard_sale.svg" alt="logo" className="logo"/>
+    
+          <form action="/" className="form">
+            <label for="email" className="label">Email address</label>
+            <input type="text" id="email" placeholder="platzi@example.cm" className="input input-email"/>
+    
+            <label for="password" className="label">Password</label>
+            <input type="password" id="password" placeholder="*********" className="input input-password"/>
+    
+            <input type="submit" value="Log in" className="primary-button login-button"/>
+            <a href="/">Forgot my password</a>
+          </form>
+    
+          <button className="secondary-button signup-button">Sign up</button>
+        </div>
+      </div>
+    );
+};
+
+export default Login;
