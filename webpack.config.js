1396e9b40ab0c3bc2cdb6a0a4ed4084e067a743d
Tulio Rangel <tjrn20@hotmail.com>
2021-12-27 20:01:20 -0500

    chande in webpack for style rules

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
                 ]
             },
             {
-                test: /\.(csss|scss)$/,
+                test: /\.(css|scss)$/,
                 use: [
                     "style-loader",
                     "css-loader",
