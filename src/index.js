9cc6190978b1dd9f6bd6849abc5b0147a7964797
Tulio Rangel <tjrn20@hotmail.com>
2021-12-28 18:17:03 -0500

    change App path in index.js main file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
-import App from "./components/App.jsx"
+import App from "./routes/App.jsx";
 
 ReactDom.render(<App />, document.getElementById("app"));
 
