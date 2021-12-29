ba3c19de56fee27aa197e01fc17470d44627265d
Tulio Rangel <tjrn20@hotmail.com>
2021-12-28 19:23:00 -0500

    add header to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
+import React from 'react';
+import Header from "../components/Header";
 
 const Home = () => {
     return(
-        <div>
-            <h1>Here will be the Home page</h1>
-        </div>
+        <Header/>
     );
 };
 
