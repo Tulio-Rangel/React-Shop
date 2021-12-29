7b636a39d2ba11c092f5ce36af7b3ed0042c4528
Tulio Rangel <tjrn20@hotmail.com>
2021-12-29 12:45:53 -0500

    add axios API

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+//es mejor usar useLayoutEffect ya que ejecuta el código una vez está renderizado, en vambio useEffect ejecuta justo antes del render
 import ProductItem from '../components/ProductItem';
 import '../styles/ProductList.scss';
+import axios from 'axios';
+
+const API = "https://api.escuelajs.co/api/v1/products";
 
 const ProductList = () => {
+	const [Products, setProducts] = useState([]);
+
+	useEffect(async () => {
+		const response = await axios(API);
+		setProducts(response.data)
+	}, []);
+
 	return (
 		<section className="main-container">
 			<div className="ProductList">
+				{Products.map(product => (
 				<ProductItem />
+				))}
 			</div>
 		</section>
 	);
