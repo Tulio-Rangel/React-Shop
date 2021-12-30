20ddc8f913dde27f02c5bc29bf57cd5963f8a540
Tulio Rangel <tjrn20@hotmail.com>
2021-12-30 18:44:11 -0500

    add toggle to shopping cart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useContext } from "react";
 import Menu from "@components/Menu"; 
+import MyOrder from "@containers/MyOrder";
 import "../styles/header.scss";
 import Logo from "../../frontend/logos/logo_yard_sale.svg";
 import IconMenu from "../../frontend/icons/icon_menu.svg";
@@ -8,6 +9,7 @@ import AppContext from "@context/AppContext";
 
 const Header = () => {
   const [toggle, setToggle] = useState(false);
+  const [toggleOrders, setToggleOrders] = useState(false);
   const { state } = useContext(AppContext);
 
   const handleToggle = () => {
@@ -45,7 +47,7 @@ const Header = () => {
       <div className="navbar-right">
         <ul>
           <li className="navbar-email" onClick={handleToggle}>amadeus@example.com</li>
-          <li className="navbar-shopping-cart">
+          <li className="navbar-shopping-cart" onClick={setToggleOrders(!toggleOrders)}>
             <img src={IconShoppingCart} alt="shopping cart" />
             {state.cart.length > 0 ? <div>{state.cart.length}</div> : null}
           </li>
@@ -53,6 +55,7 @@ const Header = () => {
       </div>
       {/* {toggle && <Menu/>} */}
       {toggle ? <Menu/> : null}
+      {toggleOrders && <MyOrder/>}
     </nav>
   );
 };
