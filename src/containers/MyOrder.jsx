ba7c04b18b0f2e67fe7135c5155c6005ae98ddf6
Tulio Rangel <tjrn20@hotmail.com>
2021-12-30 19:40:08 -0500

    add function to sum the total price and ahow it

--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -6,6 +6,13 @@ import Flechita from "../../frontend/icons/flechita.svg";
 
 const MyOrder = () => {
 	const { state } = useContext(AppContext);
+
+	const sumTotal = () => {
+		const reducer = (accumulator, currentValue) => accumulator + currentValue.price;
+		const sum = state.cart.reduce(reducer, 0);
+		return sum;
+	}
+
 	return (
 		<aside className="MyOrder">
 			<div className="title-container">
@@ -20,7 +27,7 @@ const MyOrder = () => {
 					<p>
 						<span>Total</span>
 					</p>
-					<p>$560.00</p>
+					<p>${sumTotal()}</p>
 				</div>
 				<button className="primary-button">
 					Checkout
