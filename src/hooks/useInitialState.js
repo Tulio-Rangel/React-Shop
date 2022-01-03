d376b051ff2a76cd0d25f870302a0555ac440fa0
Tulio Rangel <tjrn20@hotmail.com>
2022-01-02 19:07:00 -0500

    add function to delete just one same item not all

--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -7,10 +7,28 @@ const initialState = {
 const useInitialState = () => {
     const [state, setState] = useState(initialState);
     //el payload es como si escribiera product
+
+    const verifyIfIDExist = (id) => {
+        const idExist = state.cart.some(item => item.id === id)
+
+        if(idExist) {
+            return verifyIfIDExist(id + 1);
+        } else {
+            return id;
+        }
+    }
+
     const addToCart = (payload) => { 
+        const newID = verifyIfIDExist(payload.id)
+
+        const payloadWithNewID = {
+            ...payload,
+            id: newID
+        }
+
         setState({
             ...state,
-            cart: [...state.cart, payload]
+            cart: [...state.cart, payloadWithNewID]
         });
     };
 
