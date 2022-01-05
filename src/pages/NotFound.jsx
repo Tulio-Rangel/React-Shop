55db6765eefc1c08ef5d39d09b563d6bb891a4d8
Tulio Rangel <tjrn20@hotmail.com>
2022-01-05 18:27:05 -0500

    add semicolons

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const NotFound = () => {
     return (
@@ -11,4 +11,4 @@ const NotFound = () => {
     )
 };
 
-export default NotFound
+export default NotFound;
